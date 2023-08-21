bd9f3b5c0d62f1bf4de9b43753b86581402fe921
dhev-wisdom <nonsowisdom62@gmail.com>
2023-08-21 12:17:20 +0100

    Pagination complex

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,9 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 Module documentation
 """
 
-from typing import Tuple
+import csv
+from typing import List, Tuple
+
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
@@ -12,9 +14,10 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     if page > 0 and page_size > 0:
         start_index = (page - 1) * page_size
-        end_index = start_index + page_size - 1
+        end_index = start_index + page_size
 
         return (start_index, end_index)
+
 
 class Server:
     """
@@ -26,8 +29,9 @@ class Server:
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-    """Cached dataset
-    """
+        """
+        Cached dataset
+        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -37,4 +41,14 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        pass
+        """
+        Method uses the index_range function to return
+        sepcified number of pages from file
+        """
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+        data = self.dataset()
+        start, end = index_range(page, page_size)
+        if start > len(data):
+            return [[]]
+        return data[start:end]
