a70dd8be19e5836fbe98688b12b47021d604e1d8
dhev-wisdom <nonsowidom62@gmail.com>
2023-09-01 17:28:38 +0100

    add

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def index():
     """Home route"""
-    return render_template("index.html")
+    return render_template("0-index.html")
 
 
 if __name__ == "__main__":
---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Moldule setup a basic flask app
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config():
+    """Config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Set locale language"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """Home route"""
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
