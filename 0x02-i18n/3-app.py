06d88f996c24f28818a14a08fed4d87b6f10f735
dhev-wisdom <nonsowisdom62@gmail.com>
2023-09-01 19:19:33 +0100

    translations

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -29,7 +29,7 @@ def get_locale():
 @app.route("/", strict_slashes=False)
 def index():
     """Home route"""
-    return render_template("1-index.html")
+    return render_template("3-index.html")
 
 
 if __name__ == "__main__":
