38ea231fe12505213cc4e50da6a207b688d07fe2
dhev-wisdom <nonsowidom62@gmail.com>
2023-09-01 16:31:33 +0100

    Flask

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Moldule setup a basic flask app
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    """Home route"""
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
